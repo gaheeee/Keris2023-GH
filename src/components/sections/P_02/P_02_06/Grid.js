aad4723394caaf9597ac5d71ec53bf86a8864681
DESKTOP-1CV3TJI\eduai <ghyang@edu-ai.co.kr>
2023-07-13 14:29:29 +0900

    p0219 최종모델까지 추가

--- src/components/sections/P_02/P_02_06/Grid.js
+++ src/components/sections/P_02/P_02_06/Grid.js
@@ -1,20 +1,24 @@
 import img1 from "../../../../assets/p02/p0206_img02.jpg";
-import img1_1 from "../../../../assets/p02/p0206_img02_1.jpg";
-import img1_2 from "../../../../assets/p02/p0206_img02_2.jpg";
+import img1_1 from "../../../../assets/p02//p0206_img02_1.jpg";
+
 import img2 from "../../../../assets/p02/p0206_img03.jpg";
+import img2_1 from "../../../../assets/p02/p0206_img03_1.jpg";
+
 import img3 from "../../../../assets/p02/p0206_img04.jpg";
+import img3_1 from "../../../../assets/p02/p0206_img04_1.jpg";
+
 import img4 from "../../../../assets/p02/p0206_img05.jpg";
+import img4_1 from "../../../../assets/p02/p0206_img05_1.jpg";
+
 import { useState } from "react";
 
 export default function Grid() {
   const [selectedImage, setSelectedImage] = useState(null);
-  const [showDetectionResult, setShowDetectionResult] = useState(false);
-  const [showAccuracy, setShowAccuracy] = useState(false);
+  const [showPersonResult, setShowPersonResult] = useState(false);
 
   const openModal = (image) => {
     setSelectedImage(image);
-    setShowDetectionResult(false);
-    setShowAccuracy(false);
+    setShowPersonResult(false);
   };
 
   const closeModal = () => {
@@ -22,75 +26,60 @@ export default function Grid() {
   };
 
   const toggleResult = () => {
-    setShowDetectionResult((prevState) => !prevState);
-
+    setShowPersonResult((prevState) => !prevState);
   };
 
-  const toggleAccuracy = () => {
-    setShowAccuracy((prevState) => !prevState);
 
-  };
+  let personImage;
+
+  if (selectedImage === img1) {
+    personImage = img1_1;
+  } else if (selectedImage === img2) {
+    personImage = img2_1;
+  } else if (selectedImage === img3) {
+    personImage = img3_1;
+  } else if (selectedImage === img4) {
+    personImage = img4_1;
+  }
 
   return (
-    <div className="p0207-grid">
-
-      <div className="p0207-grid-space">
-        <div className="p0207-grid-item" onClick={() => openModal(img1)}>
+    <div className="p0206-grid">
+      <div className="p0206-grid-space">
+        <div className="p0206-grid-item" onClick={() => openModal(img1)}>
           <img src={img1} alt="img1" />
         </div>
-        <div className="p0207-grid-item" onClick={() => openModal(img2)}>
+        <div className="p0206-grid-item" onClick={() => openModal(img2)}>
           <img src={img2} alt="img2" />
         </div>
-        <div className="p0207-grid-item" onClick={() => openModal(img3)}>
+        <div className="p0206-grid-item" onClick={() => openModal(img3)}>
           <img src={img3} alt="img3" />
         </div>
-        <div className="p0207-grid-item" onClick={() => openModal(img4)}>
+        <div className="p0206-grid-item" onClick={() => openModal(img4)}>
           <img src={img4} alt="img4" />
         </div>
 
-
         {selectedImage && (
-          <div className="p0207-modal">
-            <div className="p0207-modal-content">
-              <img
-                src={selectedImage}
-                alt="modal-img"
-                className="modal-img"
-              />
+          <div className="p0206-modal">
+            <div className="p0206-modal-content" id = "p0206-modal-blank">
+              <img src={selectedImage} alt="modal-img" className="modal-img" />
 
-              {showDetectionResult && (
-                <img
-                  src={img1_1}
-                  alt="modal-img"
-                  className="detection-img"
-                />
+              {showPersonResult && (
+                <img src={personImage} alt="modal-img" className="detection-img" />
               )}
 
-              {showAccuracy && (
-                <img
-                  src={img1_2}
-                  alt="modal-img"
-                  className="accuracy-img"
-                />
-              )}
-
-              <div className="p0207-modal-btn">
-                <button onClick={toggleResult}>
-                  {showDetectionResult ? "검출 결과 감추기" : "검출 결과 보기"}
-                </button>
-                <button onClick={toggleAccuracy}>
-                  {showAccuracy ? "정확도 감추기" : "정확도 보기"}
-                </button>
-                <button className="close" onClick={closeModal}>
-                  닫기
-                </button>
+              <div className="p0206-modal-btn" >
+                  <button onClick={toggleResult}>
+                    {showPersonResult ? "사람 검출 모델" : "사람 검출 모델"}
+                  </button>
+                  <button className="close" onClick={closeModal}>
+                    뒤로
+                  </button>
+                  
               </div>
             </div>
           </div>
         )}
       </div>
-
-
     </div>
   );
 }
--- src/components/sections/P_02/P_02_11/Grid.js
+++ src/components/sections/P_02/P_02_11/Grid.js
@@ -1,96 +1,131 @@
 import img1 from "../../../../assets/p02/p0206_img02.jpg";
-import img1_1 from "../../../../assets/p02/p0206_img02_1.jpg";
+import img1_1 from "../../../../assets/p02//p0206_img02_1.jpg";
 import img1_2 from "../../../../assets/p02/p0206_img02_2.jpg";
+import img1_3 from "../../../../assets/p02/p0206_img02_5.jpg";
+
 import img2 from "../../../../assets/p02/p0206_img03.jpg";
+import img2_1 from "../../../../assets/p02/p0206_img03_1.jpg";
+import img2_2 from "../../../../assets/p02/p0206_img03_2.jpg";
+import img2_3 from "../../../../assets/p02/p0206_img03_5.jpg";
+
 import img3 from "../../../../assets/p02/p0206_img04.jpg";
+import img3_1 from "../../../../assets/p02/p0206_img04_1.jpg";
+import img3_2 from "../../../../assets/p02/p0206_img04_2.jpg";
+import img3_3 from "../../../../assets/p02/p0206_img04_5.jpg";
+
 import img4 from "../../../../assets/p02/p0206_img05.jpg";
+import img4_1 from "../../../../assets/p02/p0206_img05_1.jpg";
+import img4_2 from "../../../../assets/p02/p0206_img05_2.jpg";
+import img4_3 from "../../../../assets/p02/p0206_img05_5.jpg";
+
 import { useState } from "react";
 
 export default function Grid() {
   const [selectedImage, setSelectedImage] = useState(null);
-  const [showDetectionResult, setShowDetectionResult] = useState(false);
-  const [showAccuracy, setShowAccuracy] = useState(false);
+  const [showPersonResult, setShowPersonResult] = useState(false);
+  const [showBagResult, setShowBagResult] = useState(false);
+  const [showFinalResult, setShowFinalResult] = useState(false);
 
   const openModal = (image) => {
     setSelectedImage(image);
-    setShowDetectionResult(false);
-    setShowAccuracy(false);
+    setShowPersonResult(false);
+    setShowBagResult(false);
+    setShowFinalResult(false);
   };
 
   const closeModal = () => {
     setSelectedImage(null);
   };
 
-  const toggleResult = () => {
-    setShowDetectionResult((prevState) => !prevState);
-
+  const togglePersonResult = () => {
+    setShowPersonResult((prevState) => !prevState);
+    setShowBagResult(false);
+    setShowFinalResult(false);
   };
 
-  const toggleAccuracy = () => {
-    setShowAccuracy((prevState) => !prevState);
-
+  const toggleBagResult = () => {
+    setShowBagResult((prevState) => !prevState);
+    setShowPersonResult(false);
+    setShowFinalResult(false);
   };
+
+  const toggleFinalResult = () => {
+    setShowFinalResult((prevState) => !prevState);
+    setShowPersonResult(false);
+    setShowBagResult(false);
+  };
+
+  let personImage, bagImage, finalImage;
+
+  if (selectedImage === img1) {
+    personImage = img1_1;
+    bagImage = img1_2;
+    finalImage = img1_3;
+  } else if (selectedImage === img2) {
+    personImage = img2_1;
+    bagImage = img2_2;
+    finalImage = img2_3;
+  } else if (selectedImage === img3) {
+    personImage = img3_1;
+    bagImage = img3_2;
+    finalImage = img3_3;
+  } else if (selectedImage === img4) {
+    personImage = img4_1;
+    bagImage = img4_2;
+    finalImage = img4_3;
+  }
 
   return (
-    <div className="p0207-grid">
-
-      <div className="p0207-grid-space">
-        <div className="p0207-grid-item" onClick={() => openModal(img1)}>
+    <div className="p0206-grid">
+      <div className="p0206-grid-space">
+        <div className="p0206-grid-item" onClick={() => openModal(img1)}>
           <img src={img1} alt="img1" />
         </div>
-        <div className="p0207-grid-item" onClick={() => openModal(img2)}>
+        <div className="p0206-grid-item" onClick={() => openModal(img2)}>
           <img src={img2} alt="img2" />
         </div>
-        <div className="p0207-grid-item" onClick={() => openModal(img3)}>
+        <div className="p0206-grid-item" onClick={() => openModal(img3)}>
           <img src={img3} alt="img3" />
         </div>
-        <div className="p0207-grid-item" onClick={() => openModal(img4)}>
+        <div className="p0206-grid-item" onClick={() => openModal(img4)}>
           <img src={img4} alt="img4" />
         </div>
 
-
         {selectedImage && (
-          <div className="p0207-modal">
-            <div className="p0207-modal-content">
-              <img
-                src={selectedImage}
-                alt="modal-img"
-                className="modal-img"
-              />
+          <div className="p0206-modal">
+            <div className="p0206-modal-content">
+              <img src={selectedImage} alt="modal-img" className="modal-img" />
 
-              {showDetectionResult && (
-                <img
-                  src={img1_1}
-                  alt="modal-img"
-                  className="detection-img"
-                />
+              {showPersonResult && (
+                <img src={personImage} alt="modal-img" className="detection-img" />
               )}
 
-              {showAccuracy && (
-                <img
-                  src={img1_2}
-                  alt="modal-img"
-                  className="accuracy-img"
-                />
+              {showBagResult && (
+                <img src={bagImage} alt="modal-img" className="detection-img" />
               )}
 
-              <div className="p0207-modal-btn">
-                <button onClick={toggleResult}>
-                  {showDetectionResult ? "검출 결과 감추기" : "검출 결과 보기"}
+              {showFinalResult && (
+                <img src={finalImage} alt="modal-img" className="detection-img" />
+              )}
+
+              <div className="p0206-modal-btn">
+                <button onClick={togglePersonResult}>
+                  {showPersonResult ? "사람 검출 모델" : "사람 검출 모델"}
                 </button>
-                <button onClick={toggleAccuracy}>
-                  {showAccuracy ? "정확도 감추기" : "정확도 보기"}
+                <button onClick={toggleBagResult}>
+                  {showBagResult ? "가방 검출 모델" : "가방 검출 모델"}
+                </button>
+                <button onClick={toggleFinalResult}>
+                  {showFinalResult ? "최종 모델" : "최종 모델"}
                 </button>
                 <button className="close" onClick={closeModal}>
-                  닫기
+                  뒤로
                 </button>
               </div>
             </div>
           </div>
         )}
       </div>
-
-
     </div>
   );
 }
